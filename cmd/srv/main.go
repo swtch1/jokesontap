50c1a6560e76432f7cef0ddbd5c98db10296b656
Joshua Thornton <joshua_thornton1@homedepot.com>
2019-10-12 23:56:16 -0400

    updates

--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -27,6 +27,13 @@ var (
 )
 
 func main() {
+	// TODO: only allow GET methods on the server
+	// TODO: translate quotes like '&quot;' to real chars.
+	// TODO: set appropriate headers in the server response
+	// TODO: solve this error:
+	// ERRO[0118]/mnt/c/important/code/jokesontap/name.go:136 github.com/swtch1/jokesontap.(*BudgetNameReq).RequestOften() unable to get names from names client
+	//        error="unable to unmarshal names API response body: invalid character '<' looking for beginning of value"
+
 	cli.Init(buildVersion)
 	jokesontap.InitLogger(os.Stderr, cli.LogLevel, cli.LogFormat, cli.PrettyPrintJsonLogs)
 
