31c37fc22f59aeac98b847b554647cdd88094b88
Joshua Thornton <joshua_thornton1@homedepot.com>
2019-10-12 22:40:55 -0400

    updates

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/swtch1/jokesontap
 go 1.13
 
 require (
-	github.com/golang-collections/go-datastructures v0.0.0-20150211160725-59788d5eb259
 	github.com/pkg/errors v0.8.1
+	github.com/prometheus/client_golang v1.1.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
-	github.com/stretchr/testify v1.2.2
+	github.com/stretchr/testify v1.3.0
 )
---
+++ metric/prometheus.go
@@ -0,0 +1,42 @@
+package metric
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+var MHttpRequestTotal *prometheus.CounterVec
+
+type Prometheus struct {
+	// Port is the network port where prometheus will serve an endpoint
+	Port int
+	// set true when system is under test
+	test bool
+}
+
+// Run starts the Prometheus metrics server. Set metrics with the PrometheusMetrics.
+func (p Prometheus) Run() {
+	p.registerAllMetrics()
+	path := "/metrics"
+	log.Infof("starting prometheus at path '%s' on port '%d'", path, p.Port)
+	if !p.test {
+		mux := http.NewServeMux()
+		mux.Handle(path, promhttp.Handler())
+		go http.ListenAndServe(fmt.Sprintf(":%d", p.Port), mux)
+	}
+}
+
+// registerAllMetrics ensures all custom metrics are individually defined and registered with Prometheus.
+func (p Prometheus) registerAllMetrics() {
+	MHttpRequestTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_request_total",
+			Help: "Total number of http requests to the server.",
+		},
+		[]string{"statuscode"},
+	)
+	prometheus.MustRegister(MHttpRequestTotal)
+}
